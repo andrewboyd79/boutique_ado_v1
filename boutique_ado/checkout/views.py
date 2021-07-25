1b999dec16aff72da03705142f6243efbac9cf82
andrewboyd79 <andrewboyd79@googlemail.com>
2021-07-25 17:34:43 +0000

    Added Stripe elements functionality

--- boutique_ado/checkout/views.py
+++ boutique_ado/checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JH3yzBD8gNnPvi4Oe4lANthMin0ffy4HjejEiy6IXkmzkDsSxtQNMGaEdtD2LA4gLMOJHxDZR3dsJl5S4d5xaqo00QhE5E9k0',
+        'client_secret':'Test client secret key',
+    }
+
+
+    return render(request, template, context)
